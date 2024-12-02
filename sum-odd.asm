dab6eeb81017b6f6a1ed12dcca0bb6a4f27ceec1
12AhsanAli <ahsanmaqsood319@gmail.com>
2024-12-02 12:19:00 +0500

    new commit

--- sum-odd.asm
+++ sum-odd.asm
@@ -1,18 +1,41 @@
 include irvine32.inc
 .data
-
-; array and other variables declaration here
-
+    array1 word 4,-1,2,-7,-9,4,-15,-45
+    msg byte "Total of odd values ",0
+    msg1 byte "Total odd Numbers ",0
+    sum dword 0
+    count dword 0
 .code
 main proc
+    mov eax,0
+    mov esi,offset array1
+    mov ecx,lengthof array1
+oddnumber:
+    mov ax,word ptr[esi]
 
+    test ax,1               ;Checking if number is even
+    jz skipodd
+    movsx eax,word ptr [esi]
+    add sum,eax 
+    add count,1   
+    skipodd:    
+    add esi,type array1 
+    loop oddnumber
 
-;code here
+    mov edx,offset msg          ;Loading the address of msg 1
+    call writestring
+    call crlf
 
+    mov eax,sum                 ;Displaying the sum
+    call writeint
+    call crlf 
 
+    mov edx,offset msg1         ;Loading the address of msg 2
+    call writestring
+    call crlf
 
-exit
+    mov eax,count           ;Displaying the count
+    call writeint
+    exit
 main endp
-
-
 end main
--- sum-pos.asm
+++ sum-pos.asm
@@ -1,18 +1,43 @@
 include irvine32.inc
 .data
-
-; array and other variables declaration here
-
+array1 word 4,-1,2,-7,-9,4,-15,-45
+msg byte "Sum of Positive Numbers ",0
+msg1 byte "Total Positive Numbers ",0
+sum dword 0
+count dword 0
 .code
 main proc
 
+    mov eax,0
+    mov esi,offset array1
+    mov ecx,lengthof array1
+posino:
 
-;code here
+    mov ax,word ptr[esi]
+    test ax,8000h           ;test with 8000h to check the positive value 
+    jnz skippos
+    movsx eax,word ptr [esi]
+    add sum,eax 
+    add count,1   
 
+skippos:
 
+    add esi,type array1
+    loop posino
+    mov edx,offset msg
+    call writestring
+    call crlf
 
+    mov eax,sum         ;display the values
+    call writeint
+    call crlf 
+
+    mov edx,offset msg1 `       ;displays the value
+    call writestring
+    call crlf
+    
+    mov eax,count   
+    call writeint
 exit
 main endp
-
-
 end main
